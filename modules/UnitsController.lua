866913f70af285bac2d734b6b06d950a8caa1b68
4z0t <gastec139@gmail.com>
2022-11-28 19:30:17 +0300

    return self

--- modules/UnitsController.lua
+++ modules/UnitsController.lua
@@ -19,44 +19,56 @@ UnitsController = ClassSimple
     ---Assignes units to units controller
     ---@param self UnitsController
     ---@param units Unit[]
+    ---@return UnitsController
     Units = function(self, units)
         self.units = units
+        return self
     end,
 
     ---Assignes unit for units controller
     ---@param self UnitsController
     ---@param unit Unit
+    ---@return UnitsController
     Unit = function(self, unit)
         self.units = { unit }
+        return self
     end,
 
 
     ---Orders units to move along the chain
     ---@param self UnitsController
     ---@param chain MarkerChain
+    ---@return UnitsController
     MoveChain = function(self, chain)
         ScenarioFramework.GroupMoveChain(self.units, chain)
+        return self
     end,
 
     ---Orders units to patrol along the chain
     ---@param self UnitsController
     ---@param chain MarkerChain
+    ---@return UnitsController
     PatrolChain = function(self, chain)
         ScenarioFramework.GroupPatrolChain(self.units, chain)
+        return self
     end,
 
     ---Orders units to attack-move along the chain
     ---@param self UnitsController
     ---@param chain MarkerChain
+    ---@return UnitsController
     AttackChain = function(self, chain)
         ScenarioFramework.GroupAttackChain(self.units, chain)
+        return self
     end,
 
     ---Sets chains to be used for picking
     ---@param self UnitsController
     ---@param chains MarkerChain[]
+    ---@return UnitsController
     Chains = function(self, chains)
         self.chains = chains
+        return self
     end,
 
     ---Returns random chain from set ones
@@ -68,38 +80,46 @@ UnitsController = ClassSimple
 
     ---Orders units to attack-move along a random chain
     ---@param self UnitsController
+    ---@return UnitsController
     PickRandomAttackChain = function(self)
-        self:AttackChain(self:GetRandomChain())
+        return self:AttackChain(self:GetRandomChain())
     end,
 
     ---Orders units to move along a random chain
     ---@param self UnitsController
+    ---@return UnitsController
     PickRandomMoveChain = function(self)
-        self:MoveChain(self:GetRandomChain())
+        return self:MoveChain(self:GetRandomChain())
     end,
 
     ---Orders units to patrol along a random chain
     ---@param self UnitsController
     PickRandomPatrolChain = function(self)
-        self:PatrolChain(self:GetRandomChain())
+        return self:PatrolChain(self:GetRandomChain())
     end,
 
     ---Orders units to attack given unit
     ---@param self UnitsController
     ---@param unit Unit unit to attack
+    ---@return UnitsController
     AttackUnit = function(self, unit)
         IssueAttack(self.units, unit)
+        return self
     end,
 
     ---Orders units to patrol along a random route based on a given chain
     ---@param self any
     ---@param chain any
+    ---@return UnitsController
     RandomPatrolRoute = function(self, chain)
         ScenarioFramework.GroupPatrolRoute(self.units,
             ScenarioPlatoonAI.GetRandomPatrolRoute(ScenarioUtils.ChainToPositions(chain)))
+        return self
     end,
 
     
+
+
 
 
 
