599a86257ba259708ddf7c832f991de9f9b6a84d
4z0t <gastec139@gmail.com>
2023-02-16 00:27:17 +0300

    add units triggers

--- modules/TriggerManager.lua
+++ modules/TriggerManager.lua
@@ -45,11 +45,19 @@ BasicTrigger = Class(Runnable) {}
 ---@alias UnitCallback fun(unit: Unit):nil
 
 
-
+---Adds callbacks to units
+---@param units Unit[]
+---@param callback UnitCallback
+---@param callbackType string
+local function AddUnitsCallback(units, callback, callbackType)
+    for _, unit in units do
+        unit:AddUnitCallback(callback, callbackType)
+    end
+end
 
 ---@class IUnitTrigger
 ---@field _callback UnitCallback
----@field _type string
+---@field _type string|string[]
 IUnitTrigger = ClassSimple
 {
     _type = false,
@@ -60,22 +68,50 @@ IUnitTrigger = ClassSimple
         self._callback = callback
     end,
 
+
+
     ---Adds to units a trigger callback
     ---@param self IUnitTrigger
     ---@param units Unit[]
     Add = function(self, units)
-        error "Not implemented method IUnitTrigger.Add"
+        assert(self._type, "Not specified unit callback type!")
+
+        if type(self._type) == "string" then
+            AddUnitsCallback(units, self._callback, self._type)
+            return
+        end
+
+        for _, t in self._type do
+            AddUnitsCallback(units, self._callback, t)
+        end
     end,
 
     ---Removes from units trigger callback
     ---@param self IUnitTrigger
     ---@param units Unit[]
     Remove = function(self, units)
-        error "Not implemented method IUnitTrigger.Remove"
+        assert(self._type, "Not specified unit callback type!")
+
+        for _, unit in units do
+            unit:RemoveCallback(self._callback)
+        end
     end
 
 }
 
+
+UnitStartCaptureTrigger = Class(IUnitTrigger) { _type = 'OnStartCapture' }
+UnitStopCaptureTrigger = Class(IUnitTrigger) { _type = 'OnStopCapture' }
+UnitStartBeingCapturedTrigger = Class(IUnitTrigger) { _type = 'OnStartBeingCaptured' }
+UnitStopBeingCapturedTrigger = Class(IUnitTrigger) { _type = 'OnStopBeingCaptured' }
+UnitFailedBeingCapturedTrigger = Class(IUnitTrigger) { _type = 'OnFailedBeingCaptured' }
+UnitFailedCaptureTrigger = Class(IUnitTrigger) { _type = 'OnFailedCapture' }
+UnitStopBeingBuiltTrigger = Class(IUnitTrigger) { _type = 'OnStopBeingBuilt' }
+UnitGivenTrigger = Class(IUnitTrigger) { _type = 'OnGiven' }
+UnitVeteranTrigger = Class(IUnitTrigger) { _type = 'OnVeteran' }
+UnitFailedToBuildTrigger = Class(IUnitTrigger) { _type = 'OnFailedToBuild' }
+UnitDeathTrigger = Class(IUnitTrigger) { _type = 'OnKilled' }
+UnitDestroyedTrigger = Class(IUnitTrigger) { _type = { 'OnReclaimed', 'OnCaptured', 'OnKilled' } }
 
 
 
