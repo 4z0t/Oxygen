ada76d4f4ef65f495ee3288024862d806df26fbe
4z0t <gastec139@gmail.com>
2023-02-23 17:17:11 +0300

    add VisionHandler

--- modules/AdvancedCinematics.lua
+++ modules/AdvancedCinematics.lua
@@ -1,5 +1,6 @@
 local Cinematics = import('/lua/cinematics.lua')
 local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
+local ScenarioFramework = import('/lua/ScenarioFramework.lua')
 
 ---Enters NIS mode and after callback Exits, makes units in area(s) invincible
 ---@param callback fun()
@@ -33,4 +34,52 @@ function DisplayText(text, size, color, position, duration)
     for s in text:gfind("[^\n]+") do
         PrintText(s, size, color, duration, position)
     end
+end
+
+---@type table<string, VisionHandler>
+local visionMarkers = {}
+
+
+---@class VisionHandler
+---@field _marker VizMarker
+VisionHandler = ClassSimple
+{
+    ---inits VisionHandler
+    ---@param self VisionHandler
+    ---@param vizMarker VizMarker
+    __init = function(self, vizMarker)
+        self._marker = vizMarker
+    end,
+
+    ---Destorys VisionHandler
+    ---@param self VisionHandler
+    ---@param clearIntel? boolean
+    Destroy = function(self, clearIntel)
+        if clearIntel then ScenarioFramework.ClearIntel(Vector(self._marker.X, 0, self._marker.Z), self._marker.Radius) end
+        self._marker:Destroy()
+        self._marker = nil
+    end,
+}
+
+---Creates vision area at location of given radius for army
+---@param location Marker
+---@param radius number
+---@param armyBrain AIBrain
+---@param lifetime? number
+---@return VisionHandler
+function VisionAtLocation(location, radius, armyBrain, lifetime)
+
+    local viz = ScenarioFramework.CreateVisibleAreaLocation(radius, location, lifetime or 0, armyBrain)
+    visionMarkers[location] = VisionHandler(viz)
+
+    return visionMarkers[location]
+end
+
+---@param location Marker
+---@param clearIntel? boolean
+function ClearVisionAtLocation(location, clearIntel)
+    if visionMarkers[location] == nil then return end
+
+    visionMarkers[location]:Destroy(clearIntel)
+    visionMarkers[location] = nil
 end
