c54ec2a9031c16183abfa6227725ab31796d0f74
4z0t <gastec139@gmail.com>
2023-03-05 14:39:30 +0300

    implement NavalAttacksOpAIBuilder

--- modules/OpAIBuilders/LandAttacks.lua
+++ modules/OpAIBuilders/LandAttacks.lua
@@ -19,3 +19,45 @@
 --- | 'LightBots'
 --- | 'T1Transports'
 --- | 'T2Transports'
+
+
+
+local IOpAIBuilder = import("BasicOpAIBuilder.lua").IOpAIBuilder
+
+---@class LandAttacksOpAIBuilder : IOpAIBuilder
+LandAttacksOpAIBuilder = Class(IOpAIBuilder)
+{
+    Type = "BasicLandAttack",
+    ---Sets quantity of children for OpAI
+    ---@param self LandAttacksOpAIBuilder
+    ---@param childrenType LandAttacksChildType|LandAttacksChildType[]
+    ---@param quantity integer
+    ---@return LandAttacksOpAIBuilder
+    Quantity = function(self, childrenType, quantity)
+        return IOpAIBuilder.Quantity(self, childrenType, quantity)
+    end,
+
+    ---Enables children of OpAI
+    ---@param self LandAttacksOpAIBuilder
+    ---@param childrenType LandAttacksChildType
+    ---@return LandAttacksOpAIBuilder
+    EnableChild = function(self, childrenType)
+        return IOpAIBuilder.EnableChild(self, childrenType)
+    end,
+
+    ---Disables children of OpAI
+    ---@param self LandAttacksOpAIBuilder
+    ---@param childrenType LandAttacksChildType
+    ---@return LandAttacksOpAIBuilder
+    DisableChild = function(self, childrenType)
+        return IOpAIBuilder.DisableChild(self, childrenType)
+    end,
+
+    ---Removes children of OpAI
+    ---@param self LandAttacksOpAIBuilder
+    ---@param childrenType LandAttacksChildType|LandAttacksChildType[]
+    ---@return LandAttacksOpAIBuilder
+    RemoveChildren = function(self, childrenType)
+        return IOpAIBuilder.RemoveChildren(self, childrenType)
+    end,
+}
--- modules/OpAIBuilders/NavalAttacks.lua
+++ modules/OpAIBuilders/NavalAttacks.lua
@@ -14,3 +14,46 @@
 --- | 'T3Submarines'
 --- | 'TorpedoBoats'
 --- | 'BattleCruisers'
+
+
+
+
+local IOpAIBuilder = import("BasicOpAIBuilder.lua").IOpAIBuilder
+
+---@class NavalAttacksOpAIBuilder : IOpAIBuilder
+NavalAttacksOpAIBuilder = Class(IOpAIBuilder)
+{
+    Type = 'NavalAttacks',
+    ---Sets quantity of children for OpAI
+    ---@param self NavalAttacksOpAIBuilder
+    ---@param childrenType NavalAttacksChildType|NavalAttacksChildType[]
+    ---@param quantity integer
+    ---@return NavalAttacksOpAIBuilder
+    Quantity = function(self, childrenType, quantity)
+        return IOpAIBuilder.Quantity(self, childrenType, quantity)
+    end,
+
+    ---Enables children of OpAI
+    ---@param self NavalAttacksOpAIBuilder
+    ---@param childrenType NavalAttacksChildType
+    ---@return NavalAttacksOpAIBuilder
+    EnableChild = function(self, childrenType)
+        return IOpAIBuilder.EnableChild(self, childrenType)
+    end,
+
+    ---Disables children of OpAI
+    ---@param self NavalAttacksOpAIBuilder
+    ---@param childrenType NavalAttacksChildType
+    ---@return NavalAttacksOpAIBuilder
+    DisableChild = function(self, childrenType)
+        return IOpAIBuilder.DisableChild(self, childrenType)
+    end,
+
+    ---Removes children of OpAI
+    ---@param self NavalAttacksOpAIBuilder
+    ---@param childrenType NavalAttacksChildType|NavalAttacksChildType[]
+    ---@return NavalAttacksOpAIBuilder
+    RemoveChildren = function(self, childrenType)
+        return IOpAIBuilder.RemoveChildren(self, childrenType)
+    end,
+}
