5c841c98b7961539a9ac8bfda6053097f921feb7
4z0t <gastec139@gmail.com>
2023-03-05 14:35:42 +0300

    implement EngineerAttacksOpAIBuilder

--- modules/OpAIBuilders/EngineerAttacks.lua
+++ modules/OpAIBuilders/EngineerAttacks.lua
@@ -7,3 +7,46 @@
 --- | 'CombatEngineers'
 --- | 'T1Engineers'
 --- | 'MobileShields'
+
+
+
+
+local IOpAIBuilder = import("BasicOpAIBuilder.lua").IOpAIBuilder
+
+---@class EngineerAttacksOpAIBuilder : IOpAIBuilder
+EngineerAttacksOpAIBuilder = Class(IOpAIBuilder)
+{
+    Type = 'EngineerAttack',
+    ---Sets quantity of children for OpAI
+    ---@param self EngineerAttacksOpAIBuilder
+    ---@param childrenType EngineerAttacksChildType|EngineerAttacksChildType[]
+    ---@param quantity integer
+    ---@return EngineerAttacksOpAIBuilder
+    Quantity = function(self, childrenType, quantity)
+        return IOpAIBuilder.Quantity(self, childrenType, quantity)
+    end,
+
+    ---Enables children of OpAI
+    ---@param self EngineerAttacksOpAIBuilder
+    ---@param childrenType EngineerAttacksChildType
+    ---@return EngineerAttacksOpAIBuilder
+    EnableChild = function(self, childrenType)
+        return IOpAIBuilder.EnableChild(self, childrenType)
+    end,
+
+    ---Disables children of OpAI
+    ---@param self EngineerAttacksOpAIBuilder
+    ---@param childrenType EngineerAttacksChildType
+    ---@return EngineerAttacksOpAIBuilder
+    DisableChild = function(self, childrenType)
+        return IOpAIBuilder.DisableChild(self, childrenType)
+    end,
+
+    ---Removes children of OpAI
+    ---@param self EngineerAttacksOpAIBuilder
+    ---@param childrenType EngineerAttacksChildType|EngineerAttacksChildType[]
+    ---@return EngineerAttacksOpAIBuilder
+    RemoveChildren = function(self, childrenType)
+        return IOpAIBuilder.RemoveChildren(self, childrenType)
+    end,
+}
