d3f3cb261e664d5bfc7b5c1a0a4a65feb4e4d5fa
4z0t <gastec139@gmail.com>
2023-03-19 00:42:41 +0300

    add LocateObjective

---
+++ modules/Objectives/Locate.lua
@@ -0,0 +1,45 @@
+local CountObjective = import("IObjective.lua").CountObjective
+local ObjectiveHandlers = import("ObjectiveHandlers.lua")
+local ObjectiveArrow = import("/lua/objectivearrow.lua").ObjectiveArrow
+
+
+---@class LocateObjective : CountObjective
+---@field UnitDeathTrigger UnitDestroyedTrigger
+---@field UnitGivenTrigger UnitGivenTrigger
+---@field UnitLocatedTrigger PlayerIntelTrigger
+---@field Located table<Unit, true>
+LocateObjective = Class(CountObjective)
+{
+    Icon = "Locate",
+
+    ---@param self LocateObjective
+    OnCreate = function(self)
+        CountObjective.OnCreate(self)
+        self.Located = {}
+
+        self.UnitLocatedTrigger = Oxygen.Triggers.PlayerIntelTrigger(
+            function(unit)
+                self:OnUnitLocated(unit)
+            end
+        )
+
+        self:_UpdateUI('Progress', ("%s/%s"):format(self.Count, self.Total))
+    end,
+
+    ---@param self LocateObjective
+    ---@param unit Unit
+    OnUnitLocated = function(self, unit)
+        if not self.Active or self.Located[unit] then return end
+
+        self.Located[unit] = true
+
+        self.Count = self.Count + 1
+
+        self:_UpdateUI('Progress', ("%s/%s"):format(self.Count, self.Total))
+        self:OnProgress(self.Count, self.Total)
+
+        if self.Located == self.Total then
+            self:Success()
+        end
+    end
+}
