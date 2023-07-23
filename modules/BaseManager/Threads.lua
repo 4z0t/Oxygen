90a7c54ebd1dbd5741df3187f182e8c9be49d605
4z0t <gastec139@gmail.com>
2023-07-23 14:28:22 +0300

    Use UnitDestroyed Trigger

--- modules/BaseManager/Threads.lua
+++ modules/BaseManager/Threads.lua
@@ -1,8 +1,8 @@
-local Buff = import("/lua/sim/buff.lua")
+local Buff = import("/lua/sim/Buff.lua")
 local BaseManagerThreads = import("/lua/ai/opai/BaseManagerPlatoonThreads.lua")
 
----@type UnitDeathTrigger
-local engineerDeathTrigger = Oxygen.Triggers.UnitDeathTrigger(BaseManagerThreads.BaseManagerSingleDestroyed)
+---@type UnitDestroyedTrigger
+local engineerDestroyedTrigger = Oxygen.Triggers.UnitDestroyedTrigger(BaseManagerThreads.BaseManagerSingleDestroyed)
 
 
 --- Split the platoon into single unit platoons
@@ -19,35 +19,38 @@ function BaseManagerEngineerPlatoonSplit(platoon)
         return
     end
     for _, v in units do
-        if not v.Dead then
-            if EntityCategoryContains(categories.ENGINEER, v) and
-                bManager.EngineerQuantity > bManager.CurrentEngineerCount then
-                if bManager.EngineerBuildRateBuff then
-                    Buff.ApplyBuff(v, bManager.EngineerBuildRateBuff)
+        if v.Dead then
+            continue
+        end
+
+        if EntityCategoryContains(categories.ENGINEER, v) and
+            bManager.EngineerQuantity > bManager.CurrentEngineerCount then
+            if bManager.EngineerBuildRateBuff then
+                Buff.ApplyBuff(v, bManager.EngineerBuildRateBuff)
+            end
+
+            ---@type Platoon
+            local engPlat = aiBrain:MakePlatoon('', '')
+            aiBrain:AssignUnitsToPlatoon(engPlat, { v }, 'Support', 'None')
+            engPlat.PlatoonData = table.deepcopy(platoon.PlatoonData)
+            v.BaseName = baseName
+            engPlat:ForkAIThread(BaseManagerThreads.BaseManagerSingleEngineerPlatoon)
+
+            if not EntityCategoryContains(categories.COMMAND, v) then
+                bManager:AddCurrentEngineer()
+
+                -- Only add death callback if it hasnt been set yet
+                if not v.Subtracted then
+                    engineerDestroyedTrigger:Add(v)
                 end
 
-                ---@type Platoon
-                local engPlat = aiBrain:MakePlatoon('', '')
-                aiBrain:AssignUnitsToPlatoon(engPlat, { v }, 'Support', 'None')
-                engPlat.PlatoonData = table.deepcopy(platoon.PlatoonData)
-                v.BaseName = baseName
-                engPlat:ForkAIThread(BaseManagerThreads.BaseManagerSingleEngineerPlatoon)
-
-                if not EntityCategoryContains(categories.COMMAND, v) then
-                    bManager:AddCurrentEngineer()
-
-                    -- Only add death callback if it hasnt been set yet
-                    if not v.Subtracted then
-                        engineerDeathTrigger:Add(v)
-                    end
-
-                    -- If the base is building engineers, subtract one from the amount being built
-                    if bManager:GetEngineersBuilding() > 0 then
-                        bManager:SetEngineersBuilding(-1)
-                    end
+                -- If the base is building engineers, subtract one from the amount being built
+                if bManager:GetEngineersBuilding() > 0 then
+                    bManager:SetEngineersBuilding(-1)
                 end
             end
         end
     end
+
     aiBrain:DisbandPlatoon(platoon)
 end
