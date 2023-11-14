4c0f5d748c52f92ed70f93eec31bbafc31f0cb15
4z0t <gastec139@gmail.com>
2023-11-15 00:12:15 +0300

    initial implementation for state machine and state

--- modules/StateMachines/State.lua
+++ modules/StateMachines/State.lua
@@ -1,5 +1,97 @@
+local coroutine = coroutine
+
+---@alias StateStatus
+--- | 'exit'
+--- | 'waitfor'
+--- | 'continue'
+--- | 'return'
+--- | 'tick'
+--- | 'call'
+--- | 'next'
+
 ---@class StateBase
+---@field _state thread | false
 StateBase = ClassSimple
 {
+    ---@param self StateBase
+    __init = function(self)
+        self._state = false
+    end,
+
+    ---@param self StateBase
+    ---@param arg any
+    ---@return StateStatus
+    ---@return any
+    Resume = function(self, arg)
+        local ok, status, value
+
+        if not self._state then
+            self._state = coroutine.create(self.Run)
+            ok, status, value = coroutine.resume(self._state, self)
+        else
+            ok, status, value = coroutine.resume(self._state, arg)
+        end
+
+        if ok then
+            return status, value
+        else
+            WARN(status)
+            self:Clear()
+            return 'exit'
+        end
+    end,
+
+    ---@param self StateBase
+    Run = function(self)
+        error("Not implemented method of StateBase.Run")
+    end,
+
+    ---@param self StateBase
+    Return = function(self, value)
+        return coroutine.yield('return', value)
+    end,
+    
+    ---@param self StateBase
+    Continue = function(self)
+        return coroutine.yield('continue')
+    end,
+    
+    ---@param self StateBase
+    Tick = function(self, ticks)
+        return coroutine.yield('tick', ticks or 1)
+    end,
+    
+    ---@param self StateBase
+    WaitFor = function(self, stateName)
+        return coroutine.yield('waitfor', stateName)
+    end,
+    
+    ---@param self StateBase
+    Exit = function(self)
+        return coroutine.yield('exit')
+    end,
+    
+    ---@param self StateBase
+    Next = function (self, stateName)
+        return coroutine.yield('next', stateName)
+    end,
+    
+    ---@param self StateBase
+    Call = function (self, stateName)
+        return coroutine.yield('call', stateName)
+    end,
+
+    ---@param self StateBase
+    Clear = function (self)
+        if self._state then
+            coroutine.close(self._state)
+            self._state = nil
+        end
+    end,
+
+    ---@param self StateBase
+    Destroy = function(self)
+       self:Clear()
+    end
 
 }
--- modules/StateMachines/StateMachine.lua
+++ modules/StateMachines/StateMachine.lua
@@ -1,5 +1,87 @@
+local table = table
+
 ---@class StateMachineBase
+---@field _currentState StateBase
+---@field _states table<string, StateBase>
 StateMachineBase = ClassSimple
 {
+    ---@param self StateMachineBase
+    __init = function(self)
+        self._states = {}
+        self._currentState = nil
+    end,
 
+    ---@param self StateMachineBase
+    MainThread = function(self, startState)
+
+        self._currentState = self:ProduceState(startState)
+        self._states[startState] = self._currentState
+
+        local stateStack = {}
+
+        local status, value
+
+        while self:Condition() do
+            status, value = self._currentState:Resume(value)
+
+            if status == 'exit' then
+                break
+            elseif status == 'tick' then -- state requested waiting for certain amount of ticks
+                WaitTicks(value)
+
+            elseif status == 'continue' then
+            elseif status == 'waitfor' then
+
+            elseif status == 'next' then -- going to the next state
+                self._currentState:Destroy()
+                self._currentState = self:ProduceState(value)
+                value = nil
+
+            elseif status == 'return' then -- return the value for state that called us
+                self._currentState:Destroy()
+                self._currentState = table.remove(stateStack)
+
+            elseif status == 'call' then -- calling other state for value
+                table.insert(stateStack, self._currentState)
+                self._currentState = self:ProduceState(value)
+                value = nil
+                
+            end
+        end
+
+        self:Destroy()
+    end,
+
+    ---@param self StateMachineBase
+    Condition = function(self)
+        return true
+    end,
+
+    ---@param self StateMachineBase
+    Run = function(self, startState)
+        ForkThread(self.MainThread, self, startState)
+    end,
+
+    ---@param self StateMachineBase
+    ---@param stateName string
+    ProduceState = function(self, stateName)
+        local stateClass = self.States[stateName]
+
+        if not stateClass then
+            error(stateName .. " does not present in states of state machine")
+        end
+
+        return stateClass()
+    end,
+
+    ---@param self StateMachineBase
+    Destroy = function(self)
+        for _, state in self._states do
+            state:Destroy()
+        end
+        self._states = nil
+    end,
+
+    ---@type table<string, StateBase>
+    States = {},
 }
